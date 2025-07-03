e66de37b892b71a59df0e20e86caaec1a8486474
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-03 14:47:59 +0530

    Backend: added CUstomer Service

---
+++ backend/services/customerService.ts
@@ -0,0 +1,59 @@
+import {Prisma, PrismaClient } from "@prisma/client";
+import Customer from "../model/Customer";
+
+const prisma = new PrismaClient();
+
+export async function addCustomer(customer: Customer) {
+    console.log("Adding Customer...");
+    try{
+        await prisma.customer.create({
+            data: {
+                name: customer.name,
+                email: customer.email,
+                number: customer.number,
+                address: customer.address
+            }
+        });
+        console.log("Customer [",customer.name,"] added successfully !!!")
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function deleteCustomer(email: string){
+    try{
+        await prisma.customer.delete({
+            where: {email: email}
+        })
+        console.log('Customer Deleted : [',email,"]");
+    } catch (error) {
+        console.log("Error deleting customer", error);
+    }
+}
+
+export async function updateField(email: string, customer: Customer){
+    try{
+        await prisma.customer.update({
+            where:{email: email},
+            data:{
+                name: customer.name,
+                email: customer.email,
+                number: customer.number,
+                address: customer.address
+            }
+        })
+        console.log("Customer [",customer.name,"] updated successfully !!!")
+    } catch (error) {
+        console.log("Error updating customer",error)
+    }
+}
+
+export async function getAllCustomers(){
+    try{
+        return await prisma.customer.findMany();
+    } catch (error){
+        console.log("Error getting customers \n",error)
+    }
+}
+
+
