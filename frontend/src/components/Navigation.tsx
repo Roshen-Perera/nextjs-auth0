b8934128f04b394d822aaa0f0b348f287dcb6f83
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-04 15:42:09 +0530

    Frontend: Refactor Home and Navigation components to integrate customer session management and improve user experience

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,19 +1,36 @@
 import Link from 'next/link';
 import React from 'react'
+import { auth0 } from "@/lib/auth0";
 
-const Navigation = () => {
+const Navigation = async () => {
+  const session = await auth0.getSession();
+
   return (
     <>
       <header className="bg-gray-600 text-white shadow-md">
         <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
           <h1 className="text-2xl font-bold">Customer Manager</h1>
           <nav className="space-x-4">
-            <Link href="/" className="hover:underline">
-              Home
-            </Link>
-            <Link href="/edit" className="hover:underline">
-              Edit Customer
-            </Link>
+            {session && (
+              <>
+                <Link href="/" className="hover:underline">
+                  Home
+                </Link>
+                <Link href="/edit" className="hover:underline">
+                  Edit Customer
+                </Link>
+                <Link href="/auth/logout" className="hover:underline">
+                  Log out
+                </Link>
+              </>
+            )}
+            {!session && (
+              <>
+                <Link href="/auth/login" className="hover:underline">
+                  Log in
+                </Link>
+              </>
+            )}
           </nav>
         </div>
       </header>
