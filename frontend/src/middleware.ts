bda405af5e1e1cf489a89f60b490ecd53b0273bc
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-04 15:46:46 +0530

    Frontend: Refactor middleware to enhance session management and redirect unauthenticated users to login

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,13 +1,34 @@
-import type { NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import { auth0 } from "./lib/auth0";
 
 export async function middleware(request: NextRequest) {
     console.log('AUTH0_SECRET:', process.env.AUTH0_SECRET);
-    return await auth0.middleware(request);
+    const authRes = await auth0.middleware(request);
+
+    // authentication routes — let the middleware handle it
+    if (request.nextUrl.pathname.startsWith("/auth")) {
+        return authRes;
+    }
+
+    // public routes — no need to check for session
+    if (request.nextUrl.pathname === ("/")) {
+        return authRes;
+    }
+
+    const { origin } = new URL(request.url)
+    const session = await auth0.getSession()
+
+    // user does not have a session — redirect to login
+    if (!session) {
+        return NextResponse.redirect(`${origin}/auth/login`)
+    }
+
+    return authRes
 }
 
 export const config = {
     matcher: [
+        "/edit",
         /*
          * Match all request paths except for the ones starting with:
          * - _next/static (static files)
